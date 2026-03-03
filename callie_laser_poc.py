4672284fd1fa0bcacccfd81b9a92910a4261f851
Callie Warren <callie.warren.e@gmail.com>
2026-03-02 17:46:02 -0700

    begin german/english

--- callie_laser_poc.py
+++ callie_laser_poc.py
@@ -1,4 +1,12 @@
 from laser_encoders import LaserEncoderPipeline
+
+# python -m pip install "datasets"
+from datasets import load_dataset
+
+dataset = load_dataset("allenai/nllb", "deu_Latn-eng_Latn", streaming=True, trust_remote_code=True)
 encoder = LaserEncoderPipeline(lang="eng_Latn")
-embeddings = encoder.encode_sentences(["Hi!", "This is a sentence encoder."])
-print(embeddings.shape)  # (2, 1024)
+embeddings = []
+for i, row in enumerate(dataset["train"]):
+	embeddings.append(encoder.encode_sentences(row["translation"]["deu_Latn"]))
+	print(embeddings[0].shape)  # (2, 1024)
+	break
